cd89f9621f71472712968ae6c5919b9691ef3040
Breelyn Betts <breelynbetts@gmail.com>
2020-12-06 22:23:12 -0800

    created mat view for nut

--- backend2.0/migrations/versions/ed5cc352a4ef_.py
+++ backend2.0/migrations/versions/ed5cc352a4ef_.py
@@ -15,6 +15,8 @@ down_revision = '0feedb198686'
 branch_labels = None
 depends_on = None
 
+WATCH_TABLES = ['nut_per_100_gram', 'food_detail', 'food_unit']
+
 
 def upgrade():
     conn = op.get_bind()
@@ -42,7 +44,56 @@ def upgrade():
         )
     )
     op.create_index(op.f('ix_total_nutrition_id'), 'total_nutrition', ['food_id'], unique=True)
+    conn.execute(
+        sa.sql.text(
+            '''
+            CREATE OR REPLACE FUNCTION trig_refresh_total_nut ()
+                RETURNS TRIGGER
+                AS $$
+            BEGIN
+                REFRESH MATERIALIZED VIEW CONCURRENTLY total_nutrition;
+                RETURN NULL;
+            END;
+            $$
+            LANGUAGE plpgsql;
+            '''
+        )
+    )
+
+    for table in WATCH_TABLES:
+        conn.execute(
+            sa.sql.text(
+                '''
+                CREATE TRIGGER {table}_trigger
+                    AFTER INSERT
+                    OR UPDATE
+                    OR DELETE ON {table}
+                    FOR EACH STATEMENT
+                    EXECUTE PROCEDURE trig_refresh_total_nut  ();
+                '''.format(
+                    table=table
+                )
+            )
+        )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    # drop the materialized view
+    conn.execute(
+        sa.sql.text(
+            '''
+                DROP MATERIALIZED VIEW total_nutrition
+            '''
+        )
+    )
+    for table in WATCH_TABLES:
+        conn.execute(
+            sa.sql.text(
+                '''
+                    DROP TRIGGER IF EXISTS {table}_trigger ON {table}
+                '''.format(
+                    table=table
+                )
+            )
+        )
