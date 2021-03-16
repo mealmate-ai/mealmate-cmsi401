709be7037d95d06f0fcb38c38fa8e4b14b91b623
Breelyn Betts <breelynbetts@gmail.com>
2021-03-16 00:11:43 -0400

    added method to get current user from auth token

--- backend2.0/services/auth.py
+++ backend2.0/services/auth.py
@@ -2,4 +2,18 @@ from services import dal
 from datetime import datetime, timedelta
 import jwt
 import os
+from flask_httpauth import HTTPTokenAuth
 
+auth = HTTPTokenAuth(scheme='Bearer')
+
+token_required = auth.login_required
+
+@auth.verify_token
+def verify_token(token):
+    try:
+        payload = jwt.decode(token, os.environ.get('SECRET_KEY'), algorithms=["HS256"])
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
